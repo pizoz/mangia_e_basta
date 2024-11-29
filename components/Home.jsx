2501c6c33e96df3e920cfc40d27d7d093e953a82
pizoz <nicki.pizzocri@gmail.com>
2024-11-29 17:16:02 +0100

    Refactor Home component: remove unused imports and clean up code

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import { View, Text, Button } from 'react-native'
 import ViewModel from '../model/ViewModel';
-<<<<<<< HEAD
 import Menu from './Menu'
-
-=======
 import { useEffect } from 'react';
->>>>>>> bf180adb60eb964f0bfc9d99bcc61d571455acc9
+
 const Home = ({navigation}) => {
     useEffect(() => {
         // al caricamento della Homepage, traduco la posizione nella via in cui si trova l'utente
