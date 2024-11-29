51bef2d4ddba2a8a3a9dac82fb35c117c5ddb1e5
pizoz <nicki.pizzocri@gmail.com>
2024-11-29 17:17:38 +0100

    Refactor Home component: clean up imports and enhance user data handling

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,33 +1,25 @@
-import React from 'react'
-import { View, Text, Button } from 'react-native'
-import ViewModel from '../model/ViewModel';
-<<<<<<< HEAD
-import Menu from './Menu'
-import { useEffect } from 'react';
+import React from "react";
+import { View, Text, Button } from "react-native";
+import ViewModel from "../model/ViewModel";
+import Menu from "./Menu";
+import { useEffect } from "react";
+import { useEffect } from "react";
 
-const Home = ({navigation}) => {
-=======
+const Home = ({ navigation, route }) => {
+  useEffect(() => {
+    console.log(route.params.user);
+    // al caricamento della Homepage, traduco la posizione nella via in cui si trova l'utente
+    //ABBIAMO I MENUS
+    ViewModel.getMenus(route.params.user.sid).then((result) => {
+      console.log(result);
+    });
+  }, []);
+  return (
+    <View>
+      <Text>Home</Text>
+      <Button title="Reset" onPress={() => ViewModel.reset()} />
+    </View>
+  );
+};
 
-// import Menu from './Menu'
-
-import { useEffect } from 'react';
-
-const Home = ({navigation, route}) => {
-    
->>>>>>> 2910d759619fe3939f6cdbfd6c2827163f642cef
-    useEffect(() => {
-        console.log(route.params.user)
-        // al caricamento della Homepage, traduco la posizione nella via in cui si trova l'utente
-        //ABBIAMO I MENUS
-        ViewModel.getMenus(route.params.user.sid).then( (result) => {
-            console.log(result)
-        })
-    },[])
-    return (
-        <View>
-            <Text>Home</Text>
-            <Button title="Reset" onPress={() => ViewModel.reset()}/>
-        </View>
-    )
-}
 export default Home;
